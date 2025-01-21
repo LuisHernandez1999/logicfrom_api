cd5349245627e30f687cd909ef6d2fce771b32a9
LuisHernandez1999 <luisohernandez27@gmail.com>
2025-01-21 14:14:23 -0300

    mudanca na classe

--- classes.py
+++ classes.py
@@ -1,5 +1,3 @@
-###### aqui defino a classe que as funçoes irá trabalhar 
-
 class User:
     def __init__(self, nome, sobrenome, email, senha):
         self.nome = nome
@@ -8,19 +6,9 @@ class User:
         self.senha = senha
 
     def verificar_email(self, confirmacao_email):
-        if confirmacao_email != self.email:
-            print('Os emails não são iguais. Por favor, tente novamente.')
-            return False
-        else:
-            print('Email confirmado:', confirmacao_email)
-            return True
+        return confirmacao_email == self.email
 
     def verificar_senha(self, confirmacao_senha):
-        if confirmacao_senha != self.senha:
-            print('As senhas não são iguais. Por favor, tente novamente.')
-            return False
-        else:
-            print('Senha confirmada:', confirmacao_senha)
-            return True
+        return confirmacao_senha == self.senha
 
 
