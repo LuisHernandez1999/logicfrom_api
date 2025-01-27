454c9559352fde8961a822291f36850090046a75
LuisHernandez1999 <luisohernandez27@gmail.com>
2025-01-27 16:25:58 -0300

    jtw

--- user/app_login_user.py
+++ user/app_login_user.py
@@ -2,43 +2,52 @@ from flask import Flask, request, jsonify
 from user_functions import validar_user
 from models import User
 import sqlite3
-from datetime import datetime
+from datetime import datetime,timedelta
+import jwt
 
 app = Flask(__name__)
 
+SECRET_WORD_FOR_LOGIN = ""
 @app.route('/login', methods=['POST'])
 def api_login():
     try:
-        # Recebe os dados no formato JSON
+        # recebe os dados no formato JSON
         data = request.get_json()
         email = data.get('email')
         senha = data.get('senha')
         
-        # Verifica se ambos os campos foram preenchidos
+        # verifica se ambos os campos foram preenchidos
         if not email or not senha:
             return jsonify({"message": "Email e senha são obrigatórios"}), 400
         
-        # Valida o usuário
+        # valida o usuário
         if validar_user(email, senha):
-            # Conecta ao banco e recupera os dados do usuário
+            # conecta ao banco e recupera os dados do usuário
             with sqlite3.connect("usuarios.db") as conexao:
                 cursor = conexao.cursor()
                 cursor.execute('''SELECT * FROM users WHERE email = ?''', (email,))
                 user_data = cursor.fetchone()
 
             if user_data:
-                # Cria o objeto do usuário
+                # cria o objeto do usuário
                 user_obj = User(user_data[1], user_data[2], user_data[3], user_data[4])
                 
-                # Registra a data e hora do login
+                # registra a data e hora do login
                 data_hora_do_login = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 print(f"{user_obj.nome} fez login em {data_hora_do_login}")
                 
-                # Define o status do usuário
+                # define o status do usuário
                 status = "online e logado"
                 print(f"{user_obj.nome} está {status}")
                 
-                # Retorna a resposta
+                token_payload = {
+                    "nome": user_obj.nome,
+                    "email": user_obj.email,
+                    "exp": datetime.utcnow() + timedelta(hours=1)  # expiração do token 1hora nessa porra 
+                }
+                token = jwt.encode(token_payload, SECRET_WORD_FOR_LOGIN, algorithm="HS256")
+
+                # retorna a resposta
                 return jsonify({
                     "message": f"Login realizado com sucesso! Bem-vindo(a), {user_obj.nome}!",
                     "usuario": {
@@ -47,16 +56,17 @@ def api_login():
                         "email": user_obj.email,
                         "status": status,
                         "hora_do_login": data_hora_do_login
-                    }
+                    },
+                    "token": token
                 }), 200
             else:
                 return jsonify({"message": "Erro ao recuperar dados do usuário."}), 500
         else:
-            # Login inválido
+            # login inválido
             return jsonify({"message": "Email ou senha incorretos."}), 401
 
     except Exception as e:
-        # Tratamento de erros
+      
         return jsonify({"message": str(e)}), 500
 
 if __name__ == '__main__':
